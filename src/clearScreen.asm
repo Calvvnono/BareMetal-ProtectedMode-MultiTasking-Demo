c3938ef8bbb8d64cee98d9052901d2a82d7e4541
Calvvnono <109157902+Calvvnono@users.noreply.github.com>
2025-02-01 19:20:00 +0800

    test7

--- src/clearScreen.asm
+++ src/clearScreen.asm
@@ -3,12 +3,12 @@
     mov cx,2000
     xor di,di	 ;偏移地址
     jmp near clear
- clear:
-       mov byte [es:di],0x20	;设置文本模式内容
-       inc di
-       mov byte [es:di],0x00  ;设置文本属性
-       inc di
-       loop clear	;循环
+clear:
+    mov byte [es:di],0x20	;设置文本模式内容
+    inc di
+    mov byte [es:di],0x00  ;设置文本属性
+    inc di
+    loop clear	;循环
 
-   times 510-($-$$) db 0 ;填充0
-        db 0x55,0xaa
+times 510-($-$$) db 0 ;填充0
+    db 0x55,0xaa
